506790ffc64a4bd6312829016797a21cec9eb323
itsam-13 <abhineet.am13@gmail.com>
2026-06-26 13:48:16 +0530

    Add CRUD operations for notes: GET, POST, DELETE, and PATCH endpoints

--- day2/server.js
+++ day2/server.js
@@ -6,6 +6,10 @@ app.use(express.json()) //middleware
 let notes = []
 
 
+app.get('/notes',(req,res)=>{
+    res.json(notes) 
+})
+
 app.post('/notes', (req, res) => {
     console.log(req.body);
     notes.push(req.body)
@@ -15,6 +19,23 @@ app.post('/notes', (req, res) => {
     })
 })
 
+app.delete('/notes/:index',(req,res)=>{
+    const index = req.params.index
+    delete notes[index]
+    res.json({
+        message:"note deleted successfully"
+    })
+})
+
+app.patch('/notes/:index',(req,res)=>{
+    const index = req.params.index
+    const {title} = req.body
+    notes[index].title = title
+
+    res.json({
+        message:"updated!!"
+    })
+})
 
 
 app.listen(3000, () => {
